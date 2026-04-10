31da674802d5ff53a91fa556cda1888e5ae65b39
Saul <scorreyerop01@educarex.es>
2026-04-10 11:37:22 +0200

    ejercicio6 corregido

--- ejercicio6.py
+++ ejercicio6.py
@@ -1,9 +1,10 @@
 suma = 0
 n = int(input("Introduce un número : "))
-i = 0
+i = 1
 
 while i<=n:
-    suma = i + n
+    suma = suma + i
+    i = i+1
 
-print(f"La suma de todos los números desde " , n , " es = " , suma)
+print(f"La suma de todos los números hasta " , n , " es = " , suma)
 
